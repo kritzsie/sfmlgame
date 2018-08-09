c8fc00f7aaf6968115b0be39958e561011a2437b
Random "Kritzsie" Hobo <kritzsie@gmail.com>
2018-08-09 14:11:16 +0100

    Handle readFile failures by throwing an exception

--- src/util.cpp
+++ src/util.cpp
@@ -3,19 +3,27 @@
 #include <physfs.h>
 
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 namespace ke::util {
 std::vector<char> readFile(std::string path) {
-  PHYSFS_File* file = PHYSFS_openRead(path.c_str());
-  std::size_t size = PHYSFS_fileLength(file);
-  char* data = new char[size];
-  if (file == nullptr) {
-    std::stringstream ss;
-    ss << PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode());
-    throw std::runtime_error(ss.str());
+  if (PHYSFS_isInit() == 0) {
+    throw std::runtime_error(PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
   }
+
+  PHYSFS_File* file = PHYSFS_openRead(path.c_str());
+  if (file == nullptr) {
+    throw std::runtime_error(PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+  }
+
+  std::size_t size = PHYSFS_fileLength(file);
+  if (size == 0) {
+    return std::vector<char>();
+  }
+
+  char* data = new char[size];
   PHYSFS_readBytes(file, data, size);
   PHYSFS_close(file);
   return std::vector<char>(data, data + size);
